c1ded67e1dd5e558a85139016515b1052c075348
dewuruge <dewurugekalpadeep@gmail.com>
2023-12-07 00:40:58 +0530

    Impliment front end AddRoom,RoomTypeSelector and ApiFunctions

--- front-end/lakeSide-hotel/src/App.jsx
+++ front-end/lakeSide-hotel/src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import AddRoom from './components/room/AddRoom'
 
 function App() {
@@ -9,26 +10,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <AddRoom/>
+
     </>
   )
 }
